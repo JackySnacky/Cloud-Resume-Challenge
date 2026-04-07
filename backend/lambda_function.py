614d0f3f296e8bf39497a479116e693bfdc920ea
Jacky Chuong <jacky.chuong14@gmail.com>
2026-04-06 19:14:42 -0600

    Optimize and improve the code

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -8,26 +8,20 @@ def lambda_handler(event, context):
     # Gets the table and update the counter
     response = table.update_item(
         # Updates the item
-        Key={
-            'id': 'visitors'
-        },
+        Key={'id': 'visitors'},
         # Adds one to the count
-        UpdateExpression='ADD count :increament',
-        ExpressionAttributeValues={
-            ':increament': 1
-        }
+        UpdateExpression='ADD #count :increament',
+        # Maps a placeholder for the real attribute name, count
+        ExpressionAttributeNames={'#count': 'count'},
+        ExpressionAttributeValues={':increament': 1},
+        # Updates and gets the value of counter
+        ReturnValues="UPDATED_NEW"
     )
 
-    # Gets the item from the table and prints it
-    item = table.get_item(
-        Key={
-            'id': 'visitors'
-        }
-    )
-
-    print(item['Item'])
+    # Gets the count from the response
+    count = int(response['Attributes']['count'])
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({'count': count})
     }
