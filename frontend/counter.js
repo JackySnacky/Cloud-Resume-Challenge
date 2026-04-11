6e3a3bebe5e0c5a74f3ae372bc33821896a71cc9
Jacky Chuong <jacky.chuong14@gmail.com>
2026-04-11 07:56:35 -0600

    Remove debugging line, Added real API call counter to HTML

--- frontend/counter.js
+++ frontend/counter.js
@@ -1,15 +1,10 @@
-visitorCounterNumber = 0;
-
 async function getVisitorCount() {
     // fetch the API counter using URL
     const response = await fetch("https://9ym09kowqa.execute-api.ca-west-1.amazonaws.com/count");
     // GET method for the data
     const data = await response.json();
-    // prints the data to console for debugging
-    console.log(data);
-    visitorCounterNumber = data;
+    // Replace the visitor counter with the data
+    document.getElementById("visitor_counter").textContent = data.count;
 }
-
-console.log(visitorCounterNumber)
 
 getVisitorCount();
