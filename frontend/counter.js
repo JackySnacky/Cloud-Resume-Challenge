d21ad9dc403f5d0677ed3d3959bc16a1c81b3b7b
Jacky Chuong <jacky.chuong14@gmail.com>
2026-04-11 18:39:54 -0600

    Change API URL endpoint

--- frontend/counter.js
+++ frontend/counter.js
@@ -1,6 +1,6 @@
 async function getVisitorCount() {
     // fetch the API counter using URL
-    const response = await fetch("https://9ym09kowqa.execute-api.ca-west-1.amazonaws.com/count");
+    const response = await fetch("https://7j2gfvbtv5.execute-api.ca-west-1.amazonaws.com/count");
     // GET method for the data
     const data = await response.json();
     // Replace the visitor counter with the data
